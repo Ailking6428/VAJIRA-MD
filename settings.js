847935e88a84a5cb0ae09e4e61531e61fcf8895a
Ailking6428 <162872704+Ailking6428@users.noreply.github.com>
2024-05-29 10:07:15 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.mess = {
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 }
 
-global.owner = [process.env.OWNER_NUMBER] || ['421233418681'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = [process.env.OWNER_NUMBER] || ['923488652755'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
@@ -39,7 +39,7 @@ ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,
 WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
 ANTI_LINK: process.env.ANTI_LINK || false  ,
 ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '421233418681'  ,    
+OWNER_NUMBER: process.env.OWNER_NUMBER || '923488652755'  ,    
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || '⏤͟͟͞͞★𝐔𝐌𝐀𝐑❍₰𝐊𝐇𝐀𝐍844࿐' ,     
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
